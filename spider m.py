d51ef6cfcf7aa420f7e2fd4b58a5d060951ba66e
mariaalves1612 <141173301+mariaalves1612@users.noreply.github.com>
2023-09-05 15:15:52 -0300

    Add files via upload

---
+++ spider m.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+numero = 10
+nome = "maduh"  
+booleano = True                                             
+a = 10
+b = 5
+soma = a + b 
+subtração = a - b
+multiplicação = a * b
+divisão = a / b
+
+va = True
+vb = False
+
+r1 = va and vb
+r2 = va or vb
+r3 = not va
+
